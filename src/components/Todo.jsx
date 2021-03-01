90c3e334b1c0a646322288884342678182fd3d91
zaven-git <khzaven@gmail.com>
2021-03-01 22:41:44 +0400

    z-1 first commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoItem from './TodoItem'
+const Todo = () => {
+  return (
+    <ul>
+      <li><TodoItem /></li>
+      <li><TodoItem /></li>
+    </ul>
+  )
+}
+
+export default Todo;
